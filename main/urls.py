b24a5f9efe3887ec781f3a325d7a674db91a4fc7
arseniy_bokarev <arsenyam652@gmail.com>
2025-02-04 23:03:22 +0300

    The year validation logic has been extracted into a separate class, FourDigitYearConverter, in the converters.py file.

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path, re_path
-
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.MainPage), # http://127.0.0.1:8000
     path('cats/<int:cat_id>/', views.categories),  # http://127.0.0.1:8000/cats/1
     path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/cats/fssss
-    re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive)
+    path("archive/<year4:year>/", views.archive)
 
 ]
